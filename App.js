fdfaca897a2725fe7460ea17e5715aac41951096
matthieu-mr <m.michon.rossel@gmail.com>
2020-06-01 13:30:01 +0200

    ajout couleur et icone

--- App.js
+++ App.js
@@ -4,45 +4,62 @@ import { View, Button } from 'react-native';
 import {createAppContainer } from 'react-navigation';
 import {createStackNavigator} from 'react-navigation-stack';
 
+// library icon
+import { Ionicons } from '@expo/vector-icons';
+import { SimpleLineIcons } from '@expo/vector-icons';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+
 // Ajout des modules de navigation 
 import {createBottomTabNavigator} from 'react-navigation-tabs';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+
+
+
 
 import HomeScreen from './screens/HomeScreen'
 import Playlist from './screens/Playlist'
 import Search from './screens/Search'
 
 
-var BottomNavigator = createBottomTabNavigator({
+
+var BottomNavigator = createBottomTabNavigator({ // Creation du menu bottom avec les liens
   Home: HomeScreen,
   Playlist: Playlist,
   Search:Search
-},
-defaultNavigationOptions: ({ navigation }) => ({ // creation des icônes
-    tabBarIcon: ({ tintColor }) => {
-    var iconName;
+  
+  },
+  {
+  defaultNavigationOptions: ({ navigation }) => ({ // creation des icônes
+  tabBarIcon: ({ tintColor }) => {
+  var iconName;
     if (navigation.state.routeName == 'Home') {
-    iconName = 'ios-information-circle';
-    } else if (navigation.state.routeName == 'gallery') {
-    iconName = 'ios-photos';
-    }else if (navigation.state.routeName == 'snap') {
-    iconName = 'ios-camera';
-    }
-    return <Ionicons name={iconName} size={25} color={tintColor} />;
+      iconName = 'ios-information-circle';
+      return <SimpleLineIcons name="home" size={25} color={tintColor} />;
+
+      } else if (navigation.state.routeName == 'Playlist') {
+      iconName = 'radio';
+      return <MaterialCommunityIcons name="radio" size={24} color={tintColor} />
+
+      }else if (navigation.state.routeName == 'Search') {
+      iconName = 'ios-search';
+      return <Ionicons name={iconName} size={25} color={tintColor} />;
+      }
+   // return <Ionicons name={iconName} size={25} color={tintColor} />;
     },
-    }),
-        tabBarOptions: {
-            activeTintColor: '#009788',        
-            inactiveTintColor: '#FFFFFF',
-                style: {
-                    backgroundColor: '#111224',
-    }
-    },
-    
+  }),
+      tabBarOptions: {
+          activeTintColor: '#FFFFFF',        
+          inactiveTintColor: '#FFFFFF',
+              style: {
+                  backgroundColor: '#00838F',
+  }
+  },
+  }
   );
 
+  
 
 
-);
 
 export default Navigation = createAppContainer(BottomNavigator);
 
