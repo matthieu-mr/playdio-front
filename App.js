7b1a5deb9cc995989e0ead6e054a7e452a3c007c
matthieu-mr <m.michon.rossel@gmail.com>
2020-06-01 15:53:09 +0200

    suppr material bottom

--- App.js
+++ App.js
@@ -7,11 +7,10 @@ import {createStackNavigator} from 'react-navigation-stack';
 // library icon
 import { Ionicons } from '@expo/vector-icons';
 import { SimpleLineIcons } from '@expo/vector-icons';
-import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 // Ajout des modules de navigation 
 import {createBottomTabNavigator} from 'react-navigation-tabs';
-import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+
 
 
 
