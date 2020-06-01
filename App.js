97f5c9a2102a424de221f72606105bfd21f921bd
matthieu-mr <m.michon.rossel@gmail.com>
2020-06-01 12:24:03 +0200

    ajout menu 1

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
 console.disableYellowBox = true; 
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Button } from 'react-native';
+import {createAppContainer } from 'react-navigation';
+import {createStackNavigator} from 'react-navigation-stack';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
-}
+// Ajout des modules de navigation 
+import {createBottomTabNavigator} from 'react-navigation-tabs';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+import HomeScreen from './screens/HomeScreen'
+import Playlist from './screens/Playlist'
+import Search from './screens/Search'
+
+
+var StackNavigator = createStackNavigator({
+  Home: HomeScreen,
+  Playlist: Playlist,
+  Search:Search
 });
+
+export default Navigation = createAppContainer(StackNavigator);
+
---
+++ screens/HomeScreen.js
@@ -0,0 +1,20 @@
+console.disableYellowBox = true; 
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text> welcome homescreen</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
