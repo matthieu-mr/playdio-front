7b14762c33261e7e8009c713596c576092f84790
Marion Toutant <marion.toutant@gmail.com>
2020-06-02 12:57:21 +0200

    page home un peu plus complète

--- screens/components/Radio.js
+++ screens/components/Radio.js
@@ -4,28 +4,30 @@ import { Card, Badge } from 'react-native-elements';
 
 export default function Radio(props) {
 
-  var typeList = ["ROCK", "FUNK"]
-  var badgeList = typeList.map((type,i) => {
+  // BADGES
+  var badgeList = props.musicType.map((type,i) => {
     return (
       <Badge
         key={i}
         containerStyle={{marginLeft:1, marginRight:1}}
         value={type}
-        textStyle={{fontSize:10, marginLeft:3, marginRight:3}}
+        textStyle={{fontSize:7, marginLeft:3, marginRight:3}}
         status="primary">
       </Badge>
     )
   })
 
+  // CALLBACK & CARDS
   return (
     <View style={{flex:1, justifyContent:"center"}}>
-      <Card containerStyle={{width: 100, height: 100, borderRadius:3, shadowOpacity:1, padding: 0}}
+      <Card 
+          containerStyle={{width: 75, height: 75, marginLeft:15, marginRight:0, marginTop:8, marginBottom:0, borderRadius:3, elevation:8, shadowOffset: { width: 10, height: 10 }, shadowColor: "grey", shadowOpacity: 1, shadowRadius: 20}}
           image={require('../../assets/radio_template.jpg')}
-          imageProps={{ style: {width: 100, height: 65} }}
+          imageProps={{ style: {width: 75, height: 50} }}
           >
-          <Text style={{width:100, height:35, fontSize:13, color:"#383838"}}>{props.radioName}</Text>
+          <Text style={{width:75, height:25, fontSize:8, color:"#383838"}}>{props.radioName}</Text>
       </Card>
-      <View style={{flex:1, flexDirection:"row", justifyContent:"flex-start", marginTop:10, marginLeft:15}}>
+      <View style={{flex:1, flexDirection:"row", justifyContent:"flex-start", marginTop:5, marginLeft:15, marginBottom:0}}>
         {badgeList}
       </View>
     </View>
