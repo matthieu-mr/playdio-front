36f8846f68dc6e3909dfc8ecfe13c34353223f2f
Marion Toutant <marion.toutant@gmail.com>
2020-06-03 16:19:08 +0200

    player qui marche pas ^^

--- screens/components/Radio.js
+++ screens/components/Radio.js
@@ -1,9 +1,8 @@
 import React from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
+import { StyleSheet, View, Text, TouchableOpacity } from 'react-native';
 import { Card } from 'react-native-elements';
 import { Badge } from 'native-base';
-
-import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
 
 export default function Radio(props) {
 
@@ -12,9 +11,9 @@ export default function Radio(props) {
     return (
       <Badge
         key={i}
-        style={{ backgroundColor:'#00838F', padding:hp('0%'), marginRight:wp('1%')}}
+        style={styles.badge}
       >
-        <Text style={{color: "white", fontSize:hp('1.5%'), marginLeft:wp('1%'), marginRight:wp('1%'), marginBottom:hp('0%'), marginTop:hp('0.9%'), padding:hp('0%')}}>{type}</Text>
+        <Text style={styles.badgeText}>{type}</Text>
       </Badge>
     )
   })
@@ -24,16 +23,69 @@ export default function Radio(props) {
     <View style={{flex:1, justifyContent:"center"}}>
       <TouchableOpacity onPress={() => props.navigation.navigate('Playlist')}>
         <Card 
-            containerStyle={{width:wp('28%'), height:hp('15%'), marginLeft:wp('7%'), marginRight:wp('0%'), marginTop:hp('3%'), marginBottom:hp('0%'), borderRadius:hp('1%'), elevation:hp('3%'), shadowOffset: { width:wp('3%'), height:hp('3%') }, shadowColor: "grey", shadowOpacity: 1, shadowRadius:hp('7%')}}
+            containerStyle={styles.cardContainer}
             image={props.img}
-            imageProps={{ style: {width:wp('28%'), height:hp('10%')} }}
+            imageProps={{style: styles.cardImage}}
             >
-            <Text style={{width:wp('28%'), height:hp('5%'), fontSize:hp('2%'), color:"#383838"}}>{props.radioName}</Text>
+            <Text style={styles.cardText}>{props.radioName}</Text>
         </Card>
       </TouchableOpacity>
-      <View style={{flex:1, flexDirection:"row", justifyContent:"flex-start", marginTop:hp('1%'), marginLeft:wp('7%'), marginBottom:hp('7%')}}>
+      <View style={styles.badgeContainer}>
         {badgeList}
       </View>
     </View>
   );
 }
+
+// STYLES
+const styles = StyleSheet.create({
+  badge: {
+    backgroundColor:'#00838F',
+    padding:hp('0%'),
+    marginRight:wp('1%')
+  },
+  badgeText: {
+    color:"white", 
+    fontSize:hp('1.5%'), 
+    marginLeft:wp('1%'), 
+    marginRight:wp('1%'), 
+    marginBottom:hp('0%'), 
+    marginTop:hp('0%'), 
+    padding:hp('0%')
+  },
+  cardContainer: {
+    width:wp('28%'), 
+    height:hp('15%'), 
+    marginLeft:wp('7%'), 
+    marginRight:wp('0%'), 
+    marginTop:hp('3%'), 
+    marginBottom:hp('0%'), 
+    borderRadius:hp('1%'), 
+    elevation:hp('3%'), 
+    shadowOffset:{
+      width:wp('3%'), 
+      height:hp('3%') 
+    }, 
+    shadowColor: "grey", 
+    shadowOpacity: 1, 
+    shadowRadius:hp('7%')
+  },
+  cardImage: {
+    width:wp('28%'), 
+    height:hp('10%')
+  }, 
+  cardText: {
+    width:wp('28%'), 
+    height:hp('5%'), 
+    fontSize:hp('2%'),
+    color:"#383838"
+  }, 
+  badgeContainer: {
+    flex:1, 
+    flexDirection:"row", 
+    justifyContent:"flex-start", 
+    marginTop:hp('1%'), 
+    marginLeft:wp('7%'), 
+    marginBottom:hp('7%')
+  }
+})
