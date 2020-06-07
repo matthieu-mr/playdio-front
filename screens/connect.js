e2ac7ea5e0f9cbf2cedfdd62a05f22d1bfd3008d
matthieu-mr <m.michon.rossel@gmail.com>
2020-06-07 17:54:01 +0200

    gestion conflict

--- screens/connect.js
+++ screens/connect.js
@@ -32,13 +32,8 @@ useEffect( ()=>{
 },[])
 async function autoriseSpotify(){
 
-<<<<<<< HEAD
   var infoClientID = await fetch ('http://192.168.1.25:3000/autorisation')
   
-=======
-  var infoClientID = await fetch ('http://192.168.0.25:3000/autorisation')
-
->>>>>>> request
 
 // ip matthieu http://192.168.1.8
 // IP Marion http://192.168.1.25
